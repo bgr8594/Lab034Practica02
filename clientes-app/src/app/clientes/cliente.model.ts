754301a92c30b681a4b3b2c65f6cdda8a30a94a9
Benito <bgr8594@gmail.com>
2021-08-16 20:16:31 -0500

    Comit de practica 02

---
+++ clientes-app/src/app/clientes/cliente.model.ts
@@ -0,0 +1,13 @@
+export interface Cliente {
+    id: number;
+    nombre: string;
+    curp: string;
+    direccion: string;
+    sexo: number;
+    descipcionSexo:string;
+  }
+  
+  export interface Grupo {
+    id: number;
+    sexo: string;
+  }
---
+++ clientes-app/src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../clientes.service';
+import { Cliente, Grupo } from '../cliente.model';
+
+@Component({
+  selector: 'app-listado-clientes',
+  templateUrl: './listado-clientes.component.html',
+  styleUrls: ['./listado-clientes.component.css']
+})
+export class ListadoClientesComponent implements OnInit {
+  clientes: Cliente[]=[];
+  constructor(private clientesService: ClientesService) { }
+
+  ngOnInit(): void {
+    this.clientes = this.clientesService.getClientes();
+  }
+
+}
